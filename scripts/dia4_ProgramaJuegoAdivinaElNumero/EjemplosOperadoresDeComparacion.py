54900bb8fc2b8920c05411ea67f7409505c674ea
Javier Iranzo <jiranzo@iti.es>
2024-03-10 19:54:43 +0100

    Primera parte del día 4. Operadores de comparación

---
+++ scripts/dia4_ProgramaJuegoAdivinaElNumero/EjemplosOperadoresDeComparacion.py
@@ -0,0 +1,71 @@
+# Asignación simple
+mi_variable1 = 'Hola mundo'
+
+# Comparación
+mi_variable2 = 10 == 25
+mi_variable3 = 25 == 25
+
+# Comparacion con operaciones
+mi_variable4 = 10-5 == 8-2
+mi_variable5 = 10-5 == 7-2
+
+# Comparación con Strings
+mi_variable6 = 'blanco' == 'negro'
+mi_variable7 = 'blanco' == 'blanco'
+mi_variable8 = 'Azul' == 'azul'
+mi_variable9 = 'VERDE' == 'verde'.upper()
+
+# Comparación de tipos de datos
+mi_variable10 = '100' == 100
+mi_variable11 = 100.0 == 100
+
+# Comparación de diferencia
+mi_variable12 = 100 != 99
+mi_variable13 = 100 != 100
+
+# Comparación <, >, <=, >=
+mi_variable14 = 9 < 7
+mi_variable15 = 7 < 9
+mi_variable16 = 7 > 9
+mi_variable17 = 9 > 7
+mi_variable18 = 9 <= 9
+mi_variable19 = 9 >= 9
+
+print(type(mi_variable1))
+print(type(mi_variable2))
+print(type(mi_variable3))
+print(type(mi_variable4))
+print(type(mi_variable5))
+print(type(mi_variable6))
+print(type(mi_variable7))
+print(type(mi_variable8))
+print(type(mi_variable9))
+print(type(mi_variable10))
+print(type(mi_variable11))
+print(type(mi_variable12))
+print(type(mi_variable13))
+print(type(mi_variable14))
+print(type(mi_variable15))
+print(type(mi_variable16))
+print(type(mi_variable17))
+print(type(mi_variable18))
+print(type(mi_variable19))
+print(mi_variable1)
+print(mi_variable2)
+print(mi_variable3)
+print(mi_variable4)
+print(mi_variable5)
+print(mi_variable6)
+print(mi_variable7)
+print(mi_variable8)
+print(mi_variable9)
+print(mi_variable10)
+print(mi_variable11)
+print(mi_variable12)
+print(mi_variable13)
+print(mi_variable14)
+print(mi_variable15)
+print(mi_variable16)
+print(mi_variable17)
+print(mi_variable18)
+print(mi_variable19)
