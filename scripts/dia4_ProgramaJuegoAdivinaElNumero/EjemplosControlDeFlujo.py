080b1c341d4048ffe5de3e5df3127f351bf8821e
Javier Iranzo <jiranzo@iti.es>
2024-03-11 15:04:00 +0100

    Tercera parte del día 4. Controles de flujo

---
+++ scripts/dia4_ProgramaJuegoAdivinaElNumero/EjemplosControlDeFlujo.py
@@ -0,0 +1,32 @@
+if 10 > 9:
+    print("Es correcto")
+
+x = True
+if x: 
+    print("Es correcto")
+
+if 5 == 2: 
+    print("Es correcto")
+else: 
+    print("No es correcto")
+
+mascota = 'Pez'
+if mascota == 'Gato':
+    print('Tienes un gato')
+elif mascota == 'Pez':
+    print("Tienes un pez")
+elif mascota == 'Perro':
+    print("Tienes un perro")
+else: 
+    print("Mascota desconocida")
+
+edad = 16
+calificacion = 9;
+if edad < 18:
+    print("Eres menor de edad")
+    if calificacion >= 5:
+        print("Aprobado")
+    else:
+        print("Suspendido")
+else:
+    print("Eres mayor de edad")
