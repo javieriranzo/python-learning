1d0c6cb9b2f47578f7bd46bbdcbd482cae7507e1
Javier Iranzo <jiranzo@iti.es>
2024-03-14 16:46:09 +0100

    Ultima parte del dia 4 - Reto adivina el numero aleatorio

---
+++ scripts/dia4_ProgramaJuegoAdivinaElNumero/ProyectoAdivinaElNumero.py
@@ -0,0 +1,25 @@
+from random import randint
+
+print("### BIENVENIDO A ADIVINA UN NUMERO ###")
+nombre_jugador = input("Introduce tu nombre: ")
+print(f"Hola {nombre_jugador}, tienes que adivinar el número aleatorio entre 1 - 100 en 8 intentos")
+respuesta_jugador = 0
+intentos = 0
+numero_aleatorio = randint(1, 100)
+
+while(intentos < 8): 
+    respuesta_jugador = int(input("Introduce el número: "))
+    intentos += 1
+    if (respuesta_jugador < 1 or respuesta_jugador > 100):
+        print(f"El número {respuesta_jugador} está fuera de los rangos")
+    elif(respuesta_jugador > numero_aleatorio):
+        print(f"{respuesta_jugador} es mayor que el numero buscado")
+    elif(respuesta_jugador < numero_aleatorio):
+        print(f"{respuesta_jugador} es menor que el numero buscado")
+    else: 
+        print(f"¡Correcto! La respuesta: {respuesta_jugador} coincide con el número: {numero_aleatorio}")
+        break
+
+if(respuesta_jugador != numero_aleatorio):
+    print(f"Llevas {intentos} intentos, se te acabaron las oportunidades")   
+    print(f"El número que buscabamos era: {numero_aleatorio}") 
