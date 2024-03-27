9b950d7fdc9bc01c29c2c12243d49c92684ca10e
Javier Iranzo <jiranzo@iti.es>
2024-03-27 17:36:16 +0100

    Primera parte del día 6. Manipulación de archivos

---
+++ scripts/dia6_ProgramaUnRecetario/EjemplosAbrirManipularArchivos.py
@@ -0,0 +1,23 @@
+# Abrir archivo
+mi_archivo = open('resources/dia6_ProgramaUnRecetario/Prueba.txt')
+# Mostrar archivo. No imprime el contenido. Muestra información sobre el archivo
+print(mi_archivo)
+# Leer el archivo. No muestra el archivo
+archivo_leido = mi_archivo.read()
+# Mostrar el archivo
+print(archivo_leido)
+# Volver el puntero al principio del archivo 
+mi_archivo.seek(0)
+# Leer una linea del archivo
+linea_leida = mi_archivo.readline()
+print(linea_leida)
+linea_leida = mi_archivo.readline()
+print(linea_leida)
+linea_leida = mi_archivo.readline()
+print(linea_leida)
+# Leer todas las líneas y guardarlas en una lista
+mi_archivo.seek(0)
+lista_todas_lineas = mi_archivo.readlines()
+print(lista_todas_lineas)
+# Cerrar archivo
+mi_archivo.close()
