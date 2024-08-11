4c7b7094e836155782d5cbb6503bbf042f547294
Javier Iranzo <jiranzo@iti.es>
2024-08-12 00:32:32 +0200

    Creadas las funciones CRUD para el proyecto dia16_ProgramaUnaAplicacionWebDeTareasPendientes

--- scripts/dia16_ProgramaUnaAplicacionWebDeTareasPendientes/mis_entornos/mi_web/src/proyecto/base/urls.py
+++ scripts/dia16_ProgramaUnaAplicacionWebDeTareasPendientes/mis_entornos/mi_web/src/proyecto/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . views import ListaPendientes, DetalleTarea
+from . views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea
 
 urlpatterns = [
-    path('', ListaPendientes.as_view(), name='pendientes'),
-    path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')
+    path('', ListaPendientes.as_view(), name='tareas'),
+    path('tarea/<int:pk>', DetalleTarea.as_view(), name='detalle_tarea'),
+    path('crear_tarea/', CrearTarea.as_view(), name='crear_tarea'),
+    path('editar_tarea/<int:pk>', EditarTarea.as_view(), name='editar_tarea'),
+    path('eliminar_tarea/<int:pk>', EliminarTarea.as_view(), name='eliminar_tarea'),
 ]
--- scripts/dia16_ProgramaUnaAplicacionWebDeTareasPendientes/mis_entornos/mi_web/src/proyecto/base/views.py
+++ scripts/dia16_ProgramaUnaAplicacionWebDeTareasPendientes/mis_entornos/mi_web/src/proyecto/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . models import Tarea
 
 # Create your views here.
@@ -9,6 +11,36 @@ from . models import Tarea
 
 class ListaPendientes(ListView): 
     model = Tarea
+    context_object_name = 'lista_tareas'
+    # Si se quiere cambiar el nombre de la template se tiene que utilizar
+    # template_name = 'base/nombre_template
 
 class DetalleTarea(DetailView):
     model = Tarea
+    context_object_name = 'detalle_tarea'
+    # Si se quiere cambiar el nombre de la template se tiene que utilizar
+    # template_name = 'base/nombre_template
+
+class CrearTarea(CreateView): 
+    model = Tarea
+    # Este atributo es para que cree los campos para todos los campos del model Tarea. Si solo se quisiese campos específicos hay que pasarlos en modo lista
+    fields = '__all__'
+    # Cuando finalice la tarea con éxito, vuelve al home
+    success_url = reverse_lazy('tareas') 
+    # Si se quiere cambiar el nombre de la template se tiene que utilizar
+    # template_name = 'base/nombre_template
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    # Este atributo es para que cree los campos para todos los campos del model Tarea. Si solo se quisiese campos específicos hay que pasarlos en modo lista
+    fields = '__all__'
+    # Cuando finalice la tarea con éxito, vuelve al home
+    success_url = reverse_lazy('tareas')
+    # Si se quiere cambiar el nombre de la template se tiene que utilizar
+    # template_name = 'base/nombre_template
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'lista_tareas'
+    # Cuando finalice la tarea con éxito, vuelve al home
+    success_url = reverse_lazy('tareas')
